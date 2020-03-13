a1df3aa97f65e771965ee7f81e008ad1ccc7ca14
MPSystemsServices <jsheuer@mpss-pdx.com>
2020-03-13 15:25:30 -0700

    License Language Update
    Added license language to all source code files

--- source/e4functi.c
+++ source/e4functi.c
@@ -1,3 +1,17 @@
+/* *********************************************************************************************** */
+/* Copyright (C) 1999-2015 by Sequiter, Inc., 9644-54 Ave, NW, Suite 209, Edmonton, Alberta Canada.*/
+/* This program is free software: you can redistribute it and/or modify it under the terms of      */
+/* the GNU Lesser General Public License as published by the Free Software Foundation, version     */
+/* 3 of the License.                                                                               */
+/*                                                                                                 */
+/* This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;       */
+/* without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.       */
+/* See the GNU Lesser General Public License for more details.                                     */
+/*                                                                                                 */
+/* You should have received a copy of the GNU Lesser General Public License along with this        */
+/* program. If not, see <https://www.gnu.org/licenses/>.                                           */
+/* *********************************************************************************************** */
+
 /* e4functi.c  (c)Copyright Sequiter Software Inc., 1988-2001.  All rights reserved. */
 
 #include "d4all.h"
